89a3a08a07acb51fea63fca73b85cea3baa5471a
jtgilbert <jtgilbert89@gmail.com>
2026-05-30 19:18:00 -0600

    rm baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://jtgilbert.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ecotone_land_water/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
